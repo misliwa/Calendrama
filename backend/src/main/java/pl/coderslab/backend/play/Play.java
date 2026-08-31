c4462fe8df008b6eff3ccdb3ad7e664e92174bb1
misliwa <msliwa@visa.com>
2026-08-31 19:41:43 +0200

    added Play Entity

---
+++ backend/src/main/java/pl/coderslab/backend/play/Play.java
@@ -0,0 +1,41 @@
+package pl.coderslab.backend.play;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import pl.coderslab.backend.stage.Stage;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Play {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 1, max = 50)
+    private String title;
+
+    @NotNull
+    private String description;
+
+    @NotNull
+    private LocalDate premiereDate;
+
+    @ManyToOne
+    @NotNull
+    @JoinColumn(name = "stage_id", nullable = false)
+    Stage stage;
+
+    @NotNull
+    Integer durationInMinutes;
+}
