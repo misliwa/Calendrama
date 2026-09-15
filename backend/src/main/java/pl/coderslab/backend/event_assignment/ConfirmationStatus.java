44ffa864cb0833957ee4ab2a770a7e1bfb923203
misliwa <msliwa@visa.com>
2026-09-15 12:01:38 +0200

    created EventAssignment.java

---
+++ backend/src/main/java/pl/coderslab/backend/event_assignment/ConfirmationStatus.java
@@ -0,0 +1,5 @@
+package pl.coderslab.backend.event_assignment;
+
+public enum ConfirmationStatus {
+    CONFIRMED, PENDING, DECLINED
+}
---
+++ backend/src/main/java/pl/coderslab/backend/event_assignment/EventAssignment.java
@@ -0,0 +1,34 @@
+package pl.coderslab.backend.event_assignment;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import pl.coderslab.backend.employee.Employee;
+import pl.coderslab.backend.event.Event;
+import pl.coderslab.backend.play_staffing.PlayStaffing;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "event_assignments")
+public class EventAssignment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id", nullable = false)
+    private Event event;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @ManyToOne
+    @JoinColumn(name = "play_staffing_id", nullable = false)
+    private PlayStaffing playStaffing;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ConfirmationStatus confirmationStatus;
+}
