c6a50dafbe7c553ebc7725c0601cfad68ae468a5
misliwa <msliwa@visa.com>
2026-09-15 12:13:40 +0200

    added toDTO in EventAssignmentMapper

--- backend/src/main/java/pl/coderslab/backend/event_assignment/EventAssignmentMapper.java
+++ backend/src/main/java/pl/coderslab/backend/event_assignment/EventAssignmentMapper.java
@@ -1,7 +1,18 @@
 package pl.coderslab.backend.event_assignment;
 
+import pl.coderslab.backend.employee.EmployeeMapper;
+import pl.coderslab.backend.employee_unavailability.EmployeeUnavailabilityMapper;
+import pl.coderslab.backend.event.EventMapper;
+import pl.coderslab.backend.play_staffing.PlayStaffingMapper;
+
 public class EventAssignmentMapper {
     public EventAssignmentResponseDTO toDTO(EventAssignment eventAssignment){
-
+        return new EventAssignmentResponseDTO(
+                eventAssignment.getId(),
+                EventMapper.toDTO(eventAssignment.getEvent()),
+                EmployeeMapper.toDTO(eventAssignment.getEmployee()),
+                PlayStaffingMapper.toDTO(eventAssignment.getPlayStaffing()),
+                eventAssignment.getConfirmationStatus()
+        );
     }
 }
