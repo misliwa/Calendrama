a988da0b6888a4061cdc8444524582c58d864b75
misliwa <msliwa@visa.com>
2026-09-17 17:44:17 +0200

    deleted console logs

--- frontend/src/components/StaffingAccordion.jsx
+++ frontend/src/components/StaffingAccordion.jsx
@@ -2,50 +2,48 @@ import {Accordion, Button, Group, Select, Text} from "@mantine/core";
 import {useState} from "react";
 import StaffingAccordionItem from "./StaffingAccordionItem.jsx";
 
-function StaffingAccordion({staffingData, onEdit, onDelete, employees, onAddEmployee, onDeleteEmployee}){
-    return(
+function StaffingAccordion({staffingData, onEdit, onDelete, employees, onAddEmployee, onDeleteEmployee}) {
+    return (
         staffingData.length === 0 ? (
-                <Text c="dimmed">
-                    Nie dodano jeszcze żadnych ról ani zadań.
-                </Text>
-            ) : (
-                <Accordion variant="separated" radius="md">
-                    {staffingData.map((staffing) => {
-                        const staffingName = staffing.roleName
-                            ? `${staffing.profession.name} - ${staffing.roleName}`
-                            : staffing.profession.name;
+            <Text c="dimmed">
+                Nie dodano jeszcze żadnych ról ani zadań.
+            </Text>
+        ) : (
+            <Accordion variant="separated" radius="md">
+                {staffingData.map((staffing) => {
+                    const staffingName = staffing.roleName
+                        ? `${staffing.profession.name} - ${staffing.roleName}`
+                        : staffing.profession.name;
 
-                        const employeeOptions = employees
-                            .filter(employee =>
-                                employee.professions.some(
-                                    profession =>
-                                        profession.id === staffing.profession.id
-                                )
+                    const employeeOptions = employees
+                        .filter(employee =>
+                            employee.professions.some(
+                                profession =>
+                                    profession.id === staffing.profession.id
                             )
-                            .filter(employee => !staffing.capabilities.some(capability => capability.employee.id === employee.id))
-                            .map(employee => ({
-                                value: employee.id.toString(),
-                                label: `${employee.id}. ${employee.firstName} ${employee.lastName}`,
-                            }));
+                        )
+                        .filter(employee => !staffing.capabilities.some(capability => capability.employee.id === employee.id))
+                        .map(employee => ({
+                            value: employee.id.toString(),
+                            label: `${employee.id}. ${employee.firstName} ${employee.lastName}`,
+                        }));
 
-                        console.log(staffing);
-
-                        return (
-                            <StaffingAccordionItem
-                                key={staffing.clientId}
-                                staffing={staffing}
-                                staffingName={staffingName}
-                                employeeOptions={employeeOptions}
-                                onEdit={onEdit}
-                                onDelete={onDelete}
-                                onAddEmployee={onAddEmployee}
-                                onDeleteEmployee={onDeleteEmployee}
-                                />
-                        );
-                    })}
-                </Accordion>
-            )
+                    return (
+                        <StaffingAccordionItem
+                            key={staffing.clientId}
+                            staffing={staffing}
+                            staffingName={staffingName}
+                            employeeOptions={employeeOptions}
+                            onEdit={onEdit}
+                            onDelete={onDelete}
+                            onAddEmployee={onAddEmployee}
+                            onDeleteEmployee={onDeleteEmployee}
+                        />
+                    );
+                })}
+            </Accordion>
+        )
     )
 }
 
-export  default StaffingAccordion;
+export default StaffingAccordion;
